c97f2511652a0f21d82c000b026ff2275bd43723
Priya412-coder <priyankasingh8249@gmail.com>
2021-12-11 23:33:21 +0530

    adding UC5

--- GamblingSimulUC5.java
+++ GamblingSimulUC5.java
@@ -2,19 +2,32 @@ package com.gamblingsimulator;
 
 public class GamblingSimulUC5 {
 	public static void Gambler() {
+		int[] month = { 31 ,28 , 31 , 30 , 31 , 30 , 31 , 31 , 30 , 31 , 30 , 31 };
+		//String[] month = { "jan" , "feb" , "mar" , "apr" , "may" , "jun" , "july" , "aug" , "sept" , "oct" , "nov" , "dec" } ;
 		int stake = 100;
+		int bet = 1;
 		int won = 0;
 		int lost = 0;
-		for(int daysofmonth = 1; daysofmonth <= 30; daysofmonth++) {
+		int totalLost = 0;
+		int totalWin = 0;
+		for (int i = 0 ; i <= month.length; i++) {
+			while(stake != 50 && stake != 150) {
 			double betCheck = Math.floor(Math.random() * 10) % 2;
 			if(betCheck == 1) {
-				won++;
+				stake += bet;
+				won = stake + 100;
 			}else {
-				lost++;
+				stake -= bet;
+				lost = stake - 100;
 			}
+		System.out.println("Total amount of win in each month is " +won);	
+		System.out.println("Total amount of lost in each days is " +lost);
 		}
-		System.out.println("Total amount of win in a month is " +won+ "$");	
-		System.out.println("Total amount of lost in a month is " +lost+ "$");
+		}
+		 totalWin = totalWin + won;
+		 totalLost = totalLost + lost;
+	System.out.println("Total win for 20 days is: " + totalWin + "$");
+	System.out.println("Total lost for 20 days is: " + totalLost + "$");
 	}
 
 	public static void main(String[] args) {

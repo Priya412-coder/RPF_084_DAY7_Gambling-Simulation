ff1b3e491a6ca4084a737b66738c75e3e30dbcec
Priya412-coder <priyankasingh8249@gmail.com>
2021-12-11 23:29:36 +0530

    updating the UC3

--- GamblingSimulUC3.java
+++ GamblingSimulUC3.java
@@ -3,17 +3,23 @@ package com.gamblingsimulator;
 public class GamblingSimulUC3 {
 	public static void Gambler() {
 		int stake = 100;
-		for(int i = 1; i <= 50; i++) {
+		int bet = 1;
+		int numberOfBet = 0;
+		while(stake != 50 && stake != 150) {
 			double betCheck = Math.floor(Math.random() * 10) % 2;
 			if(betCheck == 1) {
-				System.out.println("Gambler won !");	
+				stake += bet;
+				System.out.println("Gambler won by :" +stake);	
 			}else {
-				System.out.println("oops ! Gambler lost");
+				stake -= bet;
+				System.out.println("oops ! Gambler lost by :" +stake);
 			}
+			System.out.println("Value of stake is " +stake);
 		}
 		System.out.println("Gambler is resigning for the day");
+		numberOfBet = numberOfBet + 1;
+		System.out.println("He has bet " +numberOfBet+ " times." );	
 	}
-
  public static void main(String[] args) {
 	 Gambler();
  }
